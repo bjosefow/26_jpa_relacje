f4417ff392a674957d93b3a29893351e5c06b9ad
Betina Josefowicz <betina.josefowicz@gmail.com>
2023-03-25 12:00:51 +0100

    init

---
+++ src/main/java/com/example/jpa_relacje_26_zadaniekoncowe/JpaRelacje26ZadaniekoncoweApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_relacje_26_zadaniekoncowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelacje26ZadaniekoncoweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRelacje26ZadaniekoncoweApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpa_relacje_26_zadaniekoncowe/JpaRelacje26ZadaniekoncoweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_relacje_26_zadaniekoncowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelacje26ZadaniekoncoweApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
